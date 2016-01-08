59b1e9a89ecd170825651654d8e78a572043810b
Pavel Dobranov <pdobranov@gmail.com>
2016-01-08 23:06:40 +0200

    Added product db model

---
+++ server/data/models/Product.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+module.exports.init = function() {
+  var productSchema = mongoose.Schema({
+    name: { type: String, required: true, unique: true, lowercase: true },
+    category: { type: String, required: true, lowercase: true },
+    proteins: { type: Number, required: true },
+    carbohydrates: { type: Number, required: true },
+    fats: { type: Number, required: true },
+    calories: { type: Number, required: true }
+  });
+
+  mongoose.model('Product', productSchema);
+};
