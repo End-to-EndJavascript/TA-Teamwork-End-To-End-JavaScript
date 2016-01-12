bd1236ff9a5927770034832975a46b1c7c74a05a
Pavel Dobranov <pdobranov@gmail.com>
2016-01-12 07:36:56 +0200

    removed curly braces from views data: { recipe } => recipe and { recipes } => recipes

--- server/controllers/recipes-controller.js
+++ server/controllers/recipes-controller.js
@@ -6,13 +6,13 @@ module.exports = {
   getAllRecipes: function(req, res, next) {
     recipes.getAll(req.query.page, req.query.category, req.query.sortBy)
            .then(function (recipes) {
-             res.render('recipes/all', { recipes });
+             res.render('recipes/all',  recipes );
            })
   },
   getRecipeDetails: function(req, res, next) {
     recipes.getById(req.params.id)
            .then(function (recipe) {
-             res.render('recipes/details', { recipe });
+             res.render('recipes/details',  recipe );
            });
   },
   getAddNewRecipe: function(req, res, next) {
