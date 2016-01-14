3900a31e19a49e64784fce62a2e7a036510b8a1d
Pavel Dobranov <pdobranov@gmail.com>
2016-01-14 13:24:19 +0200

    Changed baseServiceUrl to https://nodefoodapp.herokuapp.com/

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,13 @@
 (function() {
   'use strict';
 
-
   angular.module('foodApp.services', []);
   angular.module('foodApp.controllers', ['foodApp.services']);
   angular.module('foodApp', ['ui.materialize', 'foodApp.controllers'])
-    .config(['$locationProvider', function ($locationProvider) {
+    .config(['$locationProvider', function($locationProvider) {
       $locationProvider.html5Mode(true);
     }])
-    .constant('baseServiceUrl', 'http://localhost:3030/')
+    .constant('baseServiceUrl', 'https://nodefoodapp.herokuapp.com/')
     .run(['$http', function($http) {
       $http.defaults.headers.common.Accept = 'application/json';
     }]);
